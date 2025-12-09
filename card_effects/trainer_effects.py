f4e14f12a50e40ad113f477f6873aec0d8b7c3b2
Ryan Bown <rbown1993@hotmail.com>
2025-12-08 20:01:29 -0500

    added and sorted by type trainer names

--- card_effects/trainer_effects.py
+++ card_effects/trainer_effects.py
@@ -1,6 +1,14 @@
+### Supporters
+def lillie_s_determination():
+    print()
+def lana_s_aid():
+    print()
 def professor_s_research(hand, deck):
     hand.discard(len(hand))
     deck.draw(7)
+
+def boss_s_orders():
+    print()
 
 def hop(deck):
     deck.draw(3)
@@ -8,26 +16,77 @@ def hop(deck):
 def bede():
     print()
 
+def sonia():
+    print()
+
 def dan():
-    print()
-
-def evolution_incense():
-    print()
-
-def great_ball():
     print()
 
 def leon():
     print()
 
+def professor_turo_s_scenario():
+    print()
+def nessa():
+    print()
+
+
+def iono():
+    print()
+
+def arven():
+    print()
+
+def hilda():
+    print()
+
+## Items
+def prime_catcher():
+    print()
+
+def earthen_vessel():
+    print()
+def fighting_gong():
+    print()
+def premium_power_pro():
+    print()
+def superior_energy_retrieval():
+    print()
+def evolution_incense():
+    print()
+def ultra_ball():
+    print()
+def great_ball():
+    print()
+def nest_ball():
+    print()
+def counter_catcher():
+    print()
+def night_stretcher():
+    print()
+def buddy_buddy_poffin():
+    print()
+
 def ordinary_rod():
     print()
 
-def sonia():
-    print()
 
 def switch():
     print()
 
-def nessa():
+
+def super_rod():
+    print()
+
+##Tools
+
+def air_balloon():
+    print()
+def vitality_band():
+    print()
+## Stadium
+
+def jamming_tower():
+    print()
+def artazon():
     print()
